e669f8dbdd83e0420f379c23432534f06a8ee160
DariaDeg <daria.degtyarenko@exactprosystems.com>
2022-03-08 23:02:54 +0300

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by DariaDeg and{" "}
+        <a href="https://github.com/DariaDeg/Weather-app-react" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
