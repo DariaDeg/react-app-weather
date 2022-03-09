46f90e46bff112ff0b7774f52dc7957a41652ff8
DariaDeg <daria.degtyarenko@exactprosystems.com>
2022-03-09 21:13:23 +0300

    Bootstrap Added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by DariaDeg and{" "}
-        <a href="https://github.com/DariaDeg/Weather-app-react" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by DariaDeg and{" "}
+          <a
+            href="https://github.com/DariaDeg/Weather-app-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
