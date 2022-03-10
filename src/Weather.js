b663d7100a155884dce8914135bba4903f555b04
DariaDeg <daria.degtyarenko@exactprosystems.com>
2022-03-10 23:25:40 +0300

    Temperature CSS formatted

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter your city"
               classname="Form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,19 @@ export default function Weather() {
         <li>Wednesday 7:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="sun"
-          />
-          7 °C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt="sun"
+              className="float-left"
+            />
+            <span className="float-left">
+              <span className="temperature">7</span>
+              <span className="units">°C</span>
+            </span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
@@ -39,7 +50,6 @@ export default function Weather() {
           </ul>
         </div>
       </div>
-      Hello from DariaDeg
     </div>
   );
 }
