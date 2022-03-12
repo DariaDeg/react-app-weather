fc8be41e6068d10f6bc2f8dcd899bcb3407f113d
DariaDeg <daria.degtyarenko@exactprosystems.com>
2022-03-12 17:00:07 +0300

    Bootstrap Added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <div className="Container">
+      <div className="container">
         <Weather defaultCity="New York" />
         <footer>
           Coded by DariaDeg and{" "}
